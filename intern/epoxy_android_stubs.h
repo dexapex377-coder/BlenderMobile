e85ebe31c7613fd591c173002f151ae6f928370f
dexapex377-coder <dexapex377@gmail.com>
2026-06-20 06:35:07 +0000

    fix: epoxy stub should not include epoxy/gl.h (may not be in include path)

--- intern/epoxy_android_stubs.h
+++ intern/epoxy_android_stubs.h
@@ -1,6 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright 2024 Blender Foundation. All rights reserved. */
+
 #ifdef __ANDROID__
-#include <epoxy/gl.h>
-#ifndef epoxy_is_gles
-#define epoxy_is_gles() (!epoxy_is_desktop_gl())
-#endif
+#  ifndef epoxy_is_gles
+/* epoxy_is_gles was added in Epoxy 1.5.5; pre-compiled NDK libs are older.
+ * On Android we are always GLES, so the inverse of epoxy_is_desktop_gl is correct. */
+#    define epoxy_is_gles() (!epoxy_is_desktop_gl())
+#  endif
 #endif
